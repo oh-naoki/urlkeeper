47cf18aa443b9f26f5ac95ede7254b097d863939
oh-naoki <oh.naoki0819@gmail.com>
2022-09-19 21:45:52 +0900

    Add UiState and logic with dummy data

--- app/src/main/java/com/example/urlkeeper/MainActivity.kt
+++ app/src/main/java/com/example/urlkeeper/MainActivity.kt
@@ -3,26 +3,54 @@ package com.example.urlkeeper
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.activity.viewModels
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.urlkeeper.ui.theme.UrlKeeperTheme
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        viewModel.request()
         setContent {
-            UrlKeeperTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+            val uiState = viewModel.uiState
+            Box(modifier = Modifier.fillMaxSize()) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(horizontal = 16.dp)
+                        .verticalScroll(rememberScrollState()),
+                    verticalArrangement = Arrangement.spacedBy(8.dp),
                 ) {
-                    Greeting("Android")
+                    uiState.ogpList.forEach {
+                        OgpPreview(
+                            title = it.title,
+                            description = it.description,
+                            imageUrl = it.imageUrl,
+                            url = it.url,
+                            onClick = { viewModel.request() }
+                        )
+                    }
+                }
+
+                if (uiState.loading) {
+                    Text(modifier = Modifier.align(Alignment.Center), text = "Loading")
                 }
             }
         }
@@ -30,14 +58,38 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun OgpPreview(
+    title: String,
+    description: String,
+    imageUrl: String,
+    url: String,
+    onClick: (String) -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .padding(8.dp)
+            .clickable {
+                onClick(url)
+            }
+    ) {
+        AsyncImage(model = imageUrl, contentDescription = null)
+        Text(text = title, style = TextStyle(fontSize = 16.sp, fontWeight = FontWeight.Bold))
+        Text(
+            modifier = Modifier.padding(top = 4.dp),
+            text = description,
+            style = TextStyle(fontSize = 12.sp, color = Color.DarkGray)
+        )
+    }
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun DefaultPreview() {
-    UrlKeeperTheme {
-        Greeting("Android")
-    }
+fun PreviewOgpPreview() {
+    OgpPreview(
+        title = "Flutter - Build apps for any screen",
+        description = "Flutter transforms the entire app development process. Build, test, and deploy beautiful mobile, web, desktop, and embedded apps from a single codebase.",
+        imageUrl = "https://storage.googleapis.com/cms-storage-bucket/70760bf1e88b184bb1bc.png",
+        url = "https://flutter.dev/",
+        onClick = {}
+    )
 }
--- app/src/main/java/com/example/urlkeeper/MainViewModel.kt
+++ app/src/main/java/com/example/urlkeeper/MainViewModel.kt
@@ -1,8 +1,13 @@
 package com.example.urlkeeper
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -11,10 +16,36 @@ class MainViewModel @Inject constructor(
     private val ogpParseUseCase: OgpParseUseCase
 ) : ViewModel() {
 
+    var uiState by mutableStateOf(UiState())
+        private set
 
     fun request() {
         viewModelScope.launch {
-            val ogpMeta = ogpParseUseCase.getOgp("https://flutter.dev/")
+            uiState = uiState.copy(loading = true)
+            val result = listOf(
+                "https://flutter.dev/",
+                "https://flutter.dev/",
+                "https://flutter.dev/",
+                "https://flutter.dev/",
+                "https://flutter.dev/",
+                "https://flutter.dev/",
+                "https://flutter.dev/",
+                "https://flutter.dev/",
+                "https://flutter.dev/",
+                "https://flutter.dev/",
+                "https://flutter.dev/",
+                "https://flutter.dev/",
+                "https://flutter.dev/",
+                "https://flutter.dev/",
+            ).map {
+                async { ogpParseUseCase.getOgp(it) }
+            }.awaitAll()
+            uiState = uiState.copy(loading = false, ogpList = result)
         }
     }
 }
+
+data class UiState(
+    val loading: Boolean = false,
+    val ogpList: List<OgpMeta> = emptyList()
+)
